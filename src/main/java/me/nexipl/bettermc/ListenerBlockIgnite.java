6040f7cdb7ea58f09a390828e7057df0c643d3e8
Patryk360 <nexigamingyt@gmail.com>
2023-04-01 09:58:35 +0200

    0.4-SNAPSHOT

--- src/main/java/me/nexipl/bettermc/ListenerBlockIgnite.java
+++ src/main/java/me/nexipl/bettermc/ListenerBlockIgnite.java
@@ -1,21 +1,36 @@
 package me.nexipl.bettermc;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockIgniteEvent;
+//import static org.bukkit.Bukkit.getServer;
 
 import java.util.Objects;
-
-import static org.bukkit.Bukkit.getServer;
-import static org.bukkit.block.Block.getBlockKey;
 
 public class ListenerBlockIgnite implements Listener {
     @EventHandler
     public void onBlockIgnite(BlockIgniteEvent event) {
         if (Objects.equals(String.valueOf(event.getCause()), "FLINT_AND_STEEL")) {
-            getServer().getLogger().info("ok");
-            getServer().getLogger().info(String.valueOf(event.getBlock().getState().getY()));
-            getServer().getLogger().info(String.valueOf(getBlockKey(event.getBlock().getState().getX(), event.getBlock().getState().getY()-1, event.getBlock().getState().getZ())));
+            World mainWorld = Objects.requireNonNull(event.getPlayer()).getWorld();
+            Location blockL1 = new Location(mainWorld, event.getBlock().getState().getX(), event.getBlock().getState().getY()-1, event.getBlock().getState().getZ());
+            Location blockL2 = new Location(mainWorld, event.getBlock().getState().getX(), event.getBlock().getState().getY()-2, event.getBlock().getState().getZ());
+            Location blockL3 = new Location(mainWorld, event.getBlock().getState().getX(), event.getBlock().getState().getY()-3, event.getBlock().getState().getZ());
+            Location blockL4 = new Location(mainWorld, event.getBlock().getState().getX(), event.getBlock().getState().getY()-4, event.getBlock().getState().getZ());
+            Location blockL5 = new Location(mainWorld, event.getBlock().getState().getX(), event.getBlock().getState().getY()-5, event.getBlock().getState().getZ());
+
+            Location spawnZombie = new Location(mainWorld, event.getBlock().getState().getX(), event.getBlock().getState().getY()-5, event.getBlock().getState().getZ()+5);
+
+            String blockT1 = String.valueOf(blockL1.getBlock().getType());
+            String blockT2 = String.valueOf(blockL2.getBlock().getType());
+            String blockT3 = String.valueOf(blockL3.getBlock().getType());
+            String blockT4 = String.valueOf(blockL4.getBlock().getType());
+            String blockT5 = String.valueOf(blockL5.getBlock().getType());
+            if ((Objects.equals(blockT1, "QUARTZ_BLOCK")) && (Objects.equals(blockT2, "GOLD_BLOCK")) && (Objects.equals(blockT3, "GOLD_BLOCK")) && (Objects.equals(blockT4, "GOLD_BLOCK")) && (Objects.equals(blockT5, "GOLD_BLOCK"))) {
+                mainWorld.spawnEntity(spawnZombie, EntityType.ZOMBIE);
+            }
         }
     }
 }
